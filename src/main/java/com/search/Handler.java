7d5afb454fabed75e7f2ce0a5cf6c173a4f73e25
Shirish Veerabattini <shirish101k@gmail.com>
2020-08-09 18:49:43 -0500

    work/wordDict: saving work, trieNode search API is working

--- src/main/java/com/search/Handler.java
+++ src/main/java/com/search/Handler.java
@@ -15,43 +15,43 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.sample.ApiGatewayResponse;
 
-public class Handler implements RequestHandler<Map<String, String>, ApiGatewayResponse> {
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = Logger.getLogger(com.sample.Handler.class);
-	private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final Logger LOG = Logger.getLogger(com.sample.Handler.class);
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, String> input, Context context) {
-		LOG.info("received: " + input);
-		LOG.info("env variables" + System.getenv().toString());
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        LOG.info("received: " + input);
+        LOG.info("env variables" + System.getenv().toString());
 
-		try {
+        try {
 
-			Type mapType = new TypeToken<Map<String, List<String>>>() {
-			}.getType();
+            Type mapType = new TypeToken<Map<String, List<String>>>() {
+            }.getType();
 
-			Map<String, List<String>> inputBody = gson.fromJson(input.get("body"), mapType);
+            Map<String, List<String>> inputBody = gson.fromJson(input.get("body").toString(), mapType);
 
-			List<String> inputWordList = inputBody.get("queryString");
+            List<String> inputWordList = inputBody.get("queryString");
 
-			SearchAutoComplete searchAutoComplete = new SearchAutoComplete();
-			Map<String, List<String>> response = new HashMap<>();
+            SearchAutoComplete searchAutoComplete = new SearchAutoComplete();
+            Map<String, List<String>> response = new HashMap<>();
 
-			for (String inputWord : inputWordList) {
-				if (inputWord.isEmpty())
-					continue;
-				List<String> resultWords = searchAutoComplete.search(inputWord);
-				response.put(inputWord, resultWords);
-			}
+            for (String inputWord : inputWordList) {
+                if (inputWord.isEmpty())
+                    continue;
+                List<String> resultWords = searchAutoComplete.search(inputWord);
+                response.put(inputWord, resultWords);
+            }
 
-			LOG.info("response " + response.toString());
+            LOG.info("response " + response.toString());
 
-			return ApiGatewayResponse.builder().setStatusCode(200).setObjectBody(response).build();
-		} catch (Exception e) {
-			String errorMsg = "Error searching words for given request" + input.toString() + "Error:" + e.getMessage();
-			LOG.error(errorMsg);
-			return ApiGatewayResponse.builder().setStatusCode(500).setObjectBody(errorMsg)
-					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless")).build();
-		}
-	}
+            return ApiGatewayResponse.builder().setStatusCode(200).setObjectBody(response).build();
+        } catch (Exception e) {
+            String errorMsg = "Error searching words for given request" + input.toString() + "Error:" + e.getMessage();
+            LOG.error(errorMsg);
+            return ApiGatewayResponse.builder().setStatusCode(500).setObjectBody(errorMsg)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless")).build();
+        }
+    }
 }
